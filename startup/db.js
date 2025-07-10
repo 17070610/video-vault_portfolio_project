5df55dc0b447c2b0dbae8ae518d88b2600d2d083
17070610 <schimea@hotmail.com>
2025-07-10 22:12:39 +0200

    Final touches

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,12 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 winston.add(new winston.transports.Console());
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
 module.exports = function () {
-  mongoose.connect('mongodb://localhost/videovault')
-    .then(() => winston.info('MongoDB Connected...'))
+  const db = config.get('db');
+  mongoose.connect(db)
+    .then(() => winston.info(`Connected to ${db}...`))
 }
